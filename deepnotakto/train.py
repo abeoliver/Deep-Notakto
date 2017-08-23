067f8949b5aa0f1d17c6a088a1b9657e3af97b5d
abeoliver <abeoliver.116@gmail.com>
2017-08-22 21:35:13 -0400

    Training changes

--- deepnotakto/train.py
+++ deepnotakto/train.py
@@ -3,17 +3,22 @@
 # Deep Notakto Project
 
 import environment as env
-import trainer, util
-import agents.activated as activated
+import trainer, os
 import agents.Q as Q
 import agents.random_plus as random_plus
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 e = env.Env(4)
 print("Initializing Players...")
 p2 = Q.Q([16, 100, 100, 16], gamma = .3, epsilon = 0.1, beta = 3.0, name = "4x4")
-p2_train = trainer.Trainer(p1, 1e-8)
+p2_train = trainer.Trainer(p2, 1e-8)
 p1 = random_plus.RandomAgentPlus()
 print("Beginning Training...")
-e.play(p1, p2, 20000, trainer_a2 = trainer.get_episode(1e-7, rotate = True),
-       final_reward = True)
+for i in range(10000):
+	if i % 100 == 0:
+		print("{} / 1000".format(i))
+	e.play(p1, p2, 1, trainer_a2 = p2_train.get_episode(1e-7, rotate = True), \
+		final_reward = True, server_display = True)
+print("Saving params...")
+p2.save()
 print("Done")
